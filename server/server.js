99b66a050b7b5a94be16ae754b1419c35ccf2219
Marie Foxall <marie@wastedeffort.ca>
2020-08-06 14:42:25 -0400

    Exercise 1-3 : Setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("isomorphic-fetch");
 
 const app = new express();
 const port = 5678;
@@ -8,21 +9,37 @@ const port = 5678;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/spotify_access_token', (req, res, next) => {
+app.get("/spotify_access_token", (req, res, next) => {
   const clientId = process.env.SPOTIFY_CLIENT_ID;
   const clientSecret = process.env.SPOTIFY_SECRET;
 
   // We need, annoyingly, a base64-encoded string of our id:secret, for spotify.
   // We can use Buffers to do this for us.
-  const authString = Buffer.from(clientId + ':' + clientSecret).toString(
-    'base64'
+  const authString = Buffer.from(clientId + ":" + clientSecret).toString(
+    "base64"
   );
+  console.log("authString", authString);
 
+  fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${authString}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => console.log(err));
+  // app.post('https://accounts.spotify.com/api/token')
   // TODO: use authString in a request to Spotify!
-  res.send({ todo: true });
+  // res.send({ todo: true });
 });
 
-app.listen(port, function(error) {
+app.listen(port, function (error) {
   if (error) {
     console.error(error);
   } else {
